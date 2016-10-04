f43b90a3bf89fb4e5e206ca853ae3b7acd0ab898
Angela Franco Patiño <franco.angela@gmail.com>
2016-10-04 13:03:39 -0500

    init files

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    template: "<Label text='Hello workshop team!!!'></Label>",
+})
+export class AppComponent {
+
+}
